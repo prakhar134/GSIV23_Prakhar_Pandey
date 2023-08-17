d946d13e558d8585b367e91cfa64b54567ccadd5
prakhar <prakhar.ganesh.pandey@testbook.com>
2023-08-18 02:13:26 +0530

    Routing Setup

--- react-movies-app/src/index.js
+++ react-movies-app/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider, Navigate} from "react-router-dom"
+import Details from './components/Details';
+import List from './components/List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/list',
+    element: <List/>
+  },
+  {
+    path: '/details',
+    element: <Details/>
+  },
+  {
+    path: '/',
+    element: <Navigate to={'/list'}/>
+  }
+])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
