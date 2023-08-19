91df26c26718d16df0f59b713ef3ce39d527935d
prakhar <prakhar.ganesh.pandey@testbook.com>
2023-08-19 19:43:10 +0530

    ellipses and other styling done

--- react-movies-app/src/components/MovieCard.js
+++ react-movies-app/src/components/MovieCard.js
@@ -5,10 +5,15 @@ import './MovieCard.css'
 const MovieCard = ({movieData}) => {
   return (
     <div className='card'>
-        <img className='image' alt={movieData?.title} loading='lazy' src={`${MOVIE_POSTER_LINK}${movieData?.poster_path}`}/>
+        <div className='card_img'>
+          <img className='image' alt={movieData?.title} loading='lazy' src={`${MOVIE_POSTER_LINK}${movieData?.poster_path}`}/>
+        </div>
         <div className='card_data'>
-            <p>{movieData?.title}</p>
-            <p>{movieData?.overview}</p>
+          <div className='card_data_header'>
+            <p className='ellipses card_data_title'>{movieData?.title}</p>
+            <p className='rating'>{movieData?.vote_average}</p>
+          </div>
+          <p className='ellipses card_data_overview'>{movieData?.overview}</p>
         </div>
     </div>
   )
