024083bdb93c2776961ded4e1c12aaafb98df39e
prakhar <prakhar.ganesh.pandey@testbook.com>
2023-08-20 21:45:23 +0530

    deployment setup

--- react-movies-app/src/routes/List.js
+++ react-movies-app/src/routes/List.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Movies from '../components/Movies'
 import {useDispatch, useSelector} from "react-redux"
-import { clearSearchMovies, fetchMovies, searchMovie } from '../actions/movieAction'
+import {fetchMovies, searchMovie } from '../actions/movieAction'
 import "./List.css"
 import { REMOVE_MOVIES, REMOVE_SEARCH_MOVIES } from '../actions/type'
 
